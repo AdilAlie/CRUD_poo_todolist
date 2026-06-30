c0810d37fcb5e917ab34a51a4e336524b1f6aecb
AdilAlie <adil.elfater@gmail.com>
2026-06-30 08:07:25 +0000

    Nettoyage définitif et restauration de la version finale pure

--- connexion.php
+++ connexion.php
@@ -1,12 +1,18 @@
 <?php
 
-$host = '127.0.0.1';
-$dbname = 'projet_crud';
-$username = 'adil';
-$password = 'adiladil';
+// 1. ALLUMAGE DE LA MÉMOIRE (Obligatoire tout en haut)
+session_start();
+
+
+$env = parse_ini_file(__DIR__ . '/.env');
+
+$host = $env['DB_HOST'];
+$dbname = $env['DB_NAME'];
+$username = $env['DB_USER'];
+$password = $env['DB_PASS'];
 
 // On crée la variable $pdo ici. Elle contient la connexion à MariaDB.
 $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-?>
+?> 
